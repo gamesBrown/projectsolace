46f7e00f4449ad850aac343294e56d93503dcfb6
zack <zack@gamesbrown.com>
2024-08-18 19:50:44 -0400

    Created combat system

---
+++ combat.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Character
+{
+    public: 
+    bool isPlayer = false;
+    string name = "";
+    int hp = 0;
+    int stamina = 0;
+    int damage = 5;
+   
+};
+
+
+void displayMenu(Character pc, Character npc)
+{
+    cout <<
+    "Health: "<<pc.hp<<" "<<
+    "Stamina: "<<pc.stamina<<"\n"<<
+    "[Attack] [Skills]\n"
+    "[Items] [Retreat]"
+    "\n\n\n\n"
+    <<"Enemy HP: " <<npc.hp<<"\n";
+    return;
+};
+
+int attack(Character attacker, Character defender)
+{
+    defender.hp -= attacker.damage;
+    return defender.hp;
+
+};
+
+void startCombat(Character pc, Character npc)
+{
+    Character lastattack = pc;
+    while (true)
+    {
+          if(pc.hp <=0 && npc.hp <=0)
+        {
+            cout<< lastattack.name<<" has won the battle!";
+            break;
+        }
+        else if  (npc.hp <= 0)
+        {
+            cout<< pc.name<<" has won the battle!";
+            break;
+        }
+        else if  (pc.hp <= 0)
+        {
+            cout<< npc.name<<" has won the battle!";
+            break;
+        }
+        displayMenu(pc,npc);
+        cout <<"What would you like to do?\n";
+        string input = "";
+        cin >> input;
+        cout <<"Your input was: " + input + "\n";
+        if(input == "attack" ||"a"||"at")
+        {
+            cout<<"You attack the npc for "<<pc.damage<<" damage!\n";
+            npc.hp = attack(pc,npc);
+            lastattack = pc;
+            
+        }
+        else 
+        {
+            continue;
+        }
+
+        cout <<"The enemy attacks you for " << npc.damage <<" damage!\n";
+        pc.hp = attack(npc,pc);
+        lastattack = npc;
+
+      
+       
+        
+        
+    
+        
+    }
+    return;
+}
+
+/* class Npc
+{
+
+    int hp = 0;
+    int stamina = 0;
+    int damage = 5;
+}; */
+
+/* class Stats
+{
+    int hp = 0;
+    int stamina = 0;
+    int damage = 5;
+    
+
+
+}; */
+int main()
+{
+    Character pc = Character();
+    pc.hp = 25;
+    pc.stamina = 25;
+    pc.damage = 5;
+    pc.name = "Venom Viper";
+    Character npc = Character();
+    npc.hp = 25;
+    npc.stamina = 25;
+    npc.damage = 10;
+    npc.name = "Edmund";
+
+
+    startCombat(pc,npc);
+    return 0;
+}
