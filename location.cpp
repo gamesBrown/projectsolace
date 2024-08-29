2b8494bb812e8e9a256b199e03fe6e78922e4b40
zack <zack@gamesbrown.com>
2024-08-29 15:28:28 -0400

    added locations,encounter pulls from location for enemies

---
+++ location.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "character.h"
+
+using namespace std;
+
+
+class Location
+{
+    public:
+    std::string name;
+    Character enemiesPresent[1];
+    //Map class
+    
+};
+
+
